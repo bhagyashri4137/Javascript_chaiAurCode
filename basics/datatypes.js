bdbfd3ea2e7a6997c7a2cc88394b9b09d1b1660b
BhagyashriBhendarkar <bhagyashri4137@gmail.com>
2024-07-19 07:03:28 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,14 @@
+//undefined = value not assigned typeof undefined is undefined  
+
+//null = absence of any object value TYPEOF NULL IS OBJECT
+//For example, if you expect an object to be assigned to a variable later, 
+//you might initialize it with null to indicate that it's currently empty but not undefined.
+let user = null; // user is explicitly empty
+// Later in the code
+user = { name: "Alice" }; // user is now assigned an object
+
+
+let fullName = "Bhagyashri";
+console.log(typeof fullName);
+
+//symbol - for unique value
--- basics/test.js
+++ basics/test.js
@@ -1,1 +1,6 @@
-console.log("Bhagyashri")
+const accountID = 12345;
+let Email = "Bhagyashri@gmail.com";
+var accountNo = '123456789';
+let password;
+console.log(accountID, Email, accountNo, password);
+console.table([accountID, Email, accountNo, password]);
